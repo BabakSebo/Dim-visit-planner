e8d536491e22b610accb087e54b9a546b4353e38
BabakSebo <87017743+BabakSebo@users.noreply.github.com>
2025-04-15 13:27:41 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set, get, child, update } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+import { getDatabase, ref, set, get, child, update } from "firebase/database"; // Correct import for database
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app); // Initialize the Firebase Realtime Database
+
 const analytics = getAnalytics(app);
 
 export { app, database, ref, set, get, child, update };
